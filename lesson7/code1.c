132cf4a936a1c95a9a61d26e13691523a17a89de
1836191788 <1836191788@qq.com>
2020-06-22 23:14:21 +0800

    “第七次作业by葛立山”

---
+++ lesson7/code1.c
@@ -0,0 +1,39 @@
+ #include <MsTimer2.h>
+ int tick=0;
+  byte a=0;
+ void mytimer()
+ {
+ 	a=tick;
+    digitalWrite(8,HIGH);
+    digitalWrite(1,a&0x1);
+    digitalWrite(3,(a>>1)&0x1);
+    digitalWrite(4,(a>>2)&0x1);
+    digitalWrite(5,(a>>3)&0x1);
+    digitalWrite(8,LOW);
+  	delay(1000);
+    if(tick==9)
+      tick=-1;
+ 	tick++;
+	 
+ }
+
+void setup()
+{
+  pinMode (1,OUTPUT);
+  pinMode (3,OUTPUT);
+  pinMode (4,OUTPUT);
+  pinMode (5,OUTPUT);
+  pinMode (8,OUTPUT);
+  digitalWrite(8,LOW);
+  pinMode(6,OUTPUT);//²âÊÔ
+  pinMode(7,OUTPUT);//ÏûÒþ
+  
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  MsTimer2::set(1000,mytimer);
+  MsTimer2::start();
+}
+void loop()
+{
+  
+}
